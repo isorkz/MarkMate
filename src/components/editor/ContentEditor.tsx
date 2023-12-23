b13f9e94c1980d2d1e78c30a09d2cc1a985ea105
Isorkz <isorkz@outlook.com>
2023-12-23 15:33:57 +0800

    Render markdown content

--- src/components/editor/ContentEditor.tsx
+++ src/components/editor/ContentEditor.tsx
@@ -5,6 +5,8 @@ import { languages } from '@codemirror/language-data';
 import StyledMarkdown from './StyledMarkdown';
 import { BaseEditor, Descendant, Editor, Transforms, createEditor, Element as SlateElement, Text, } from 'slate'
 import { Slate, RenderElementProps, RenderLeafProps, Editable, ReactEditor, withReact } from 'slate-react'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import { unified } from 'unified'
 import remarkParse from 'remark-parse'
 import remarkGfm from 'remark-gfm'
@@ -23,28 +25,31 @@ type ListItemElement = { type: 'list-item', children: Descendant[], checked?: bo
 type CodeElement = { type: 'code', children: Descendant[], language?: string, render?: boolean }
 type CodeInlineElement = { type: 'code-line', children: Descendant[], num?: number }
 type ImageElement = { type: 'image', url: string, children: Descendant[] }
-type LinkElement = { type: 'link', url: string, children: Descendant[] }
+// type LinkElement = { type: 'link', url: string, children: Descendant[] }
 type HrElement = { type: 'hr', children: Descendant[] }   // Element must have children property, even though HrElement doesn't need it. Otherwise it will throw exceptions for Transforms.insertNodes.
-type StrongElement = { type: 'strong', children: Descendant[] }
-type EmphasisElement = { type: 'emphasis', children: Descendant[] }
-type DeleteElement = { type: 'delete', children: Descendant[] }
 type BlockQuoteElement = { type: 'blockquote', children: Descendant[] }
 type TableElement = { type: 'table', children: Descendant[] }
 type TableRowElement = { type: 'table-row', children: Descendant[] }
-type TableCellElement = { type: 'table-cell', children: Descendant[] }
+type TableCellElement = { type: 'table-cell', children: Descendant[], isFirstRow?: boolean }
 type FootnoteReferenceElement = { type: 'footnoteReference', identifier?: string, label?: string, children: Descendant[] }
 type FootnoteDefinitionElement = { type: 'footnoteDefinition', identifier?: string, label?: string, children: Descendant[] }
+type HtmlElement = { type: 'html', value: string, children: Descendant[] }
 
 type CustomText = {
   text: string,
   isInlineCode?: boolean,
+  url?: string,
+  bold?: boolean,
+  emphasis?: boolean,
+  delete?: boolean,
+  html?: boolean,
 }
 
 declare module 'slate' {
   interface CustomTypes {
     Editor: BaseEditor & ReactEditor
-    Element: ParagraphElement | HeadElement | ListElement | ListItemElement | CodeElement | CodeInlineElement | ImageElement | LinkElement | StrongElement | EmphasisElement | DeleteElement
-    | BlockQuoteElement | TableElement | TableRowElement | TableCellElement | HrElement | FootnoteReferenceElement | FootnoteDefinitionElement
+    Element: ParagraphElement | HeadElement | ListElement | ListItemElement | CodeElement | CodeInlineElement | ImageElement
+    | BlockQuoteElement | TableElement | TableRowElement | TableCellElement | HrElement | FootnoteReferenceElement | FootnoteDefinitionElement | HtmlElement
     Text: CustomText
   }
 }
@@ -53,6 +58,42 @@ const parseMarkdownToSlate = (mdContent: string) => {
   const parser = unified().use(remarkParse).use(remarkGfm)
   const mdast = parser.parse(mdContent)
   return mdastToSlate(mdast.children)
+}
+
+const parseLeafElement = (astNode: any, slateCustomText: CustomText) => {
+  switch (astNode.type) {
+    case 'text':
+      slateCustomText.text = astNode.value
+      break;
+    case 'strong':
+      slateCustomText.bold = true
+      break;
+    case 'emphasis':
+      slateCustomText.emphasis = true
+      break;
+    case 'delete':
+      slateCustomText.delete = true
+      break;
+    case 'link':
+      slateCustomText.url = astNode.url
+      break;
+    case 'inlineCode':
+      slateCustomText.isInlineCode = true
+      slateCustomText.text = astNode.value
+      break;
+    case 'html':
+      slateCustomText.text = astNode.value
+      break;
+    default:
+      console.error(`Unknown node type in parseLeafElements: ${JSON.stringify(astNode)}`);
+  }
+
+  if (astNode.children) {
+    for (let child of astNode.children) {
+      parseLeafElement(child, slateCustomText)
+    }
+  }
+  return slateCustomText
 }
 
 // Parse markdown AST to Slate nodes.
@@ -66,11 +107,6 @@ const mdastToSlate = (mdastNodes: any[]) => {
         slateNodes.push({
           type: 'paragraph',
           children: mdastToSlate(node.children),
-        })
-        break;
-      case 'text':
-        slateNodes.push({
-          text: node.value,
         })
         break;
       case 'heading':
@@ -108,42 +144,11 @@ const mdastToSlate = (mdastNodes: any[]) => {
           }),
         })
         break;
-      case 'inlineCode':
-        slateNodes.push({
-          text: node.value,
-          isInlineCode: true,
-        })
-        break;
       case 'image':
         slateNodes.push({
           type: 'image',
           url: decodeURIComponent(node.url),
           children: [{ text: '' }],
-        })
-        break;
-      case 'link':
-        slateNodes.push({
-          type: 'link',
-          url: decodeURIComponent(node.url),
-          children: [{ text: '' }],
-        })
-        break;
-      case 'delete':
-        slateNodes.push({
-          type: 'delete',
-          children: mdastToSlate(node.children),
-        })
-        break;
-      case 'strong':
-        slateNodes.push({
-          type: 'strong',
-          children: mdastToSlate(node.children),
-        })
-        break;
-      case 'emphasis':
-        slateNodes.push({
-          type: 'emphasis',
-          children: mdastToSlate(node.children),
         })
         break;
       case 'blockquote':
@@ -155,8 +160,20 @@ const mdastToSlate = (mdastNodes: any[]) => {
       case 'table':
         slateNodes.push({
           type: 'table',
-          children: mdastToSlate(node.children),
-        })
+          children: node.children.map((row: any, i: number) => {
+            return {
+              type: 'table-row',
+              children: row.children.map((cell: any, j: number) => {
+                return {
+                  type: 'table-cell',
+                  // Treat the first row as the table header.
+                  isFirstRow: i === 0,
+                  children: mdastToSlate(cell.children),
+                }
+              }),
+            }
+          }),
+        });
         break;
       case 'tableRow':
         slateNodes.push({
@@ -197,8 +214,20 @@ const mdastToSlate = (mdastNodes: any[]) => {
           children: [{ text: '' }],
         })
         break;
+      case 'html':
+        slateNodes.push({
+          type: 'html',
+          value: node.value,
+          children: [{ text: '' }],
+        })
+        break;
       default:
-        throw new TypeError(`Unknown node type: ${node}`);
+        if (['strong', 'link', 'text', 'emphasis', 'delete', 'inlineCode'].includes(node.type)) {
+          let slateCustomText: CustomText = { text: '' }
+          slateNodes.push(parseLeafElement(node, slateCustomText))
+        } else {
+          console.error(`Unknown node type in mdastToSlate: ${JSON.stringify(node)}`);
+        }
     }
   }
 
@@ -207,11 +236,91 @@ const mdastToSlate = (mdastNodes: any[]) => {
 
 const Element = ({ attributes, children, element }: RenderElementProps) => {
   switch (element.type) {
-    case 'image':
-      return <img src={element.url}>{children}</img>
-    default:
+    case 'paragraph':
       return <p {...attributes}>{children}</p>
+    case 'head':
+      const Head = `h${element.level}`;
+      return <Head {...attributes}>{children}</Head>
+    case 'list':
+      if (element.order) return <ol {...attributes}>{children}</ol>
+      else return <ul {...attributes}>{children}</ul>
+    case 'list-item':
+      return <li {...attributes}>{children}</li>
+    case 'image':
+      return <img src={element.url}></img>
+    case 'code':
+      let language = element.language || 'text'
+      if (element.language === 'c++') {
+        language = 'cpp'
+      }
+      return (
+        <div style={{ position: 'relative' }}>
+          <SyntaxHighlighter
+            language={language}
+            style={vscDarkPlus}
+            customStyle={{
+              overflowX: 'auto',
+              borderRadius: '5px',
+              // minWidth: '100px',
+              // maxWidth: '600px',
+              // width: '300px',
+            }}
+            className={"MarkMateCodeBlocks"}
+            children={element.children.map((item: any) => item.children[0]?.text).join('\n')} />
+          {/* show the language name on the top right corner. */}
+          <div style={{
+            position: 'absolute',
+            top: '0',
+            right: '0',
+            padding: '5px',
+            color: '#aaa',
+            fontSize: '12px',
+          }}>
+            {language}
+          </div>
+        </div>
+      )
+    case 'blockquote':
+      return <blockquote {...attributes}>{children}</blockquote>
+    case 'hr':
+      return <hr />
+    case 'table':
+      const head = children[0];  // 第一行作为表头
+      const body = children.slice(1);  // 其他行作为表体
+      return (
+        <table {...attributes}>
+          <thead>{head}</thead>
+          <tbody>{body}</tbody>
+        </table>
+      );
+    case 'table-row':
+      return <tr {...attributes}>{children}</tr>
+    case 'table-cell':
+      if (element.isFirstRow) return <th {...attributes}>{children}</th>
+      else return <td {...attributes}>{children}</td>
+    default:
+      return <div {...attributes}>{children}</div>
   }
+}
+
+const Leaf = (props: RenderLeafProps) => {
+  let children = <>{props.children}</>
+  if (props.leaf.url) {
+    children = <a href={props.leaf.url} className='underline'>{children}</a>
+  }
+  if (props.leaf.bold) {
+    children = <strong>{children}</strong>
+  }
+  if (props.leaf.emphasis) {
+    children = <em>{children}</em>
+  }
+  if (props.leaf.delete) {
+    children = <del>{children}</del>
+  }
+  if (props.leaf.isInlineCode) {
+    children = <code>{children}</code>
+  }
+  return <span {...props.attributes}>{children}</span>
 }
 
 const ContentEditor = ({
@@ -229,12 +338,14 @@ const ContentEditor = ({
   ])
 
   const cleanupSlate = () => {
-    Transforms.delete(editor, {
-      at: {
-        anchor: Editor.start(editor, []),
-        focus: Editor.end(editor, []),
-      },
-    });
+    if (editor.children.length > 0) {
+      Transforms.delete(editor, {
+        at: {
+          anchor: Editor.start(editor, []),
+          focus: Editor.end(editor, []),
+        },
+      });
+    }
   }
 
   useEffect(() => {
@@ -249,13 +360,15 @@ const ContentEditor = ({
 
   const renderElement = useCallback((props: RenderElementProps) => <Element {...props} />, [])
 
+  const renderLeaf = useCallback((props: RenderLeafProps) => <Leaf {...props} />, [])
+
   return (
     // using 'break-all' to break the long words
     <div className="flex h-full w-full overflow-y-auto overflow-x-hidden">
-      <div className="flex h-full w-full px-[10%] py-[5%] overflow-x-auto">
+      <div className="flex h-full w-full px-[10%] py-[5%] mb-[5%] overflow-x-auto">
         <div className='w-full break-all MarkMateContent'>
-          <Slate editor={editor} initialValue={slateContent} onChange={value => { setSlateContent(value) }}>
-            <Editable renderElement={renderElement} />
+          <Slate editor={editor} initialValue={slateContent} onChange={value => { setSlateContent(value) }} >
+            <Editable renderElement={renderElement} renderLeaf={renderLeaf} style={{ border: 'none', boxShadow: 'none', outline: 'none' }} />
           </Slate>
           {/* <CodeMirror value={content} extensions={[markdown({ base: markdownLanguage, codeLanguages: languages })]} onChange={onChange} /> */}
           {/* <StyledMarkdown>{mdSourceContent}</StyledMarkdown> */}
