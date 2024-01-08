6f5fc123b3d648a4bc3bdac261f5e3a53a853cd3
Isorkz <isorkz@outlook.com>
2024-01-08 20:31:50 +0800

    setup file tree by opened file

--- src/components/file-tree/FileTree.tsx
+++ src/components/file-tree/FileTree.tsx
@@ -6,20 +6,19 @@ type TreeNode = {
   name: string;
   path: string;
   children?: TreeNode[];
+  isOpened?: boolean;
 };
 
 interface TreeNodeProps {
   node: TreeNode;
   level?: number;
-  isRoot?: boolean;
 };
 
 const TreeNode = ({
   node,
   level = 0,
-  isRoot = false,
 }: TreeNodeProps) => {
-  const [opened, setOpened] = useState(isRoot);
+  const [opened, setOpened] = useState(node.isOpened || false);
   const currentDocument = useStore((state) => state.currentDocument);
   const setCurrentDocument = useStore((state) => state.setCurrentDocument);
 
@@ -44,7 +43,7 @@ const TreeNode = ({
 
   return (
     <div>
-      <div className="flex items-center hover:bg-neutral-700" style={{ paddingLeft: `${level}em` }} onClick={handleClick}>
+      <div className={`flex items-center hover:bg-neutral-700 ${node.path === currentDocument?.filePath && 'bg-neutral-600'}`} style={{ paddingLeft: `${level}em` }} onClick={handleClick}>
         {node.children && (opened ? <ChevronDownIcon className="w-4 h-4 mr-1.5" /> : <ChevronRightIcon className="w-4 h-4 mr-1.5" />)}
         <span className="font-medium text-gray-300">{node.name}</span>
       </div>
@@ -57,20 +56,45 @@ const TreeNode = ({
 };
 
 const FileTree = () => {
-  const [data, setData] = useState<TreeNode>();
+  const [tree, setTree] = useState<TreeNode>();
   const dirPath = useStore((state) => state.dirPath);
+  const currentDocument = useStore((state) => state.currentDocument);
+
+  const openNodeAtPath = (node: TreeNode | undefined, openedFile: string | undefined): boolean => {
+    if (node && openedFile) {
+      if (node.path === openedFile) {
+        node.isOpened = true
+        return true
+      }
+
+      if (node.children) {
+        for (const child of node.children) {
+          if (openNodeAtPath(child, openedFile)) {
+            node.isOpened = true;
+            return true;
+          }
+        }
+      }
+    }
+    return false
+  }
 
   useEffect(() => {
-    if (!dirPath) return;
-    // window.api defined in preload.ts, and implemented in ipcHandler.ts
-    window.api.readDirTree(dirPath).then(setData);
+    console.log('[FileTree] dirPath has changed: ', dirPath)
+    if (dirPath) {
+      // window.api defined in preload.ts, and implemented in ipcHandler.ts
+      window.api.readDirTree(dirPath).then((treeData: any) => {
+        openNodeAtPath(treeData, currentDocument?.filePath)
+        setTree(treeData);
+      });
+    }
   }, [dirPath]);
 
   return (
     // flex-grow: allow a flex item to grow and shrink as needed, then it will push the settings to the bottom
     <div className="flex flex-col flex-grow w-full overflow-y-auto overflow-x-hidden m-2">
       <ul>
-        {data ? <TreeNode node={data} isRoot /> : <div className="p-5 text-center text-gray-500">Loading...</div>}
+        {tree?.isOpened ? <TreeNode node={tree} /> : <div className="p-5 text-center text-gray-500">Loading...</div>}
       </ul>
     </div>
   );
