b401976af7cfdf1f109a07c99df9990eb1dd8403
Isorkz <isorkz@outlook.com>
2024-09-24 17:11:06 +0800

    Support html parser

--- src/components/editor/slate/plugin/withInsertData.ts
+++ src/components/editor/slate/plugin/withInsertData.ts
@@ -1,6 +1,5 @@
 import { Editor, Transforms, Element as SlateElement } from 'slate'
-import { CustomText, DefaultParagraphElement, ImageElement } from '../Element';
-import { isValidUrl } from '../../../../utils/common';
+import { DefaultParagraphElement, ImageElement } from '../Element';
 
 // TODO: handle if rootDir / currentFilePath is undefined
 export const withInsertData = (editor: Editor, rootDir: string | undefined, currentFilePath: string | undefined) => {
@@ -14,12 +13,10 @@ export const withInsertData = (editor: Editor, rootDir: string | undefined, curr
   // This is a proxy method to call in this order insertFragmentData(editor: ReactEditor, data: DataTransfer) and then insertTextData(editor: ReactEditor, data: DataTransfer).
   // Wiki: https://docs.slatejs.org/libraries/slate-react/react-editor#datatransfer-methods
   editor.insertData = data => {
-    let text = data.getData('text/plain')
-    const { files } = data
-
     console.log('[insertData] data: ', data)
-    console.log('[insertData] text: ', text)
 
+    // If only insert image
+    const { files } = data
     if (files && files.length > 0) {
       for (const file of files) {
         const reader = new FileReader()
@@ -50,12 +47,26 @@ export const withInsertData = (editor: Editor, rootDir: string | undefined, curr
       }
     }
 
+    
+
+    let html = data.getData('text/html')
+    console.log('[insertData] html: ', html)
+    // If the html is not pasted from slate editor, parse it to slate nodes.
+    // Otherwise, use the default insertData method.
+    if (html && !html.includes('data-slate-')) {
+      const slateNodes = parseHtml(html)
+      Transforms.insertFragment(editor, slateNodes);
+      return
+    }
+
     // Only insert the text in code block. Because the default insertData method will add extra code block format.
     if (editor.selection) {
       const [match] = Editor.nodes(editor, {
         match: n => SlateElement.isElement(n) && n.type === 'code',
       })
       if (match) {
+        let text = data.getData('text/plain')
+        console.log('[insertData] text: ', text)
         // For text copied from code block, remove the extra new line.
         text = text.replace(/\n\n/g, '\n')
         Transforms.insertFragment(editor, text.split('\n').map(lineText => {
@@ -64,23 +75,6 @@ export const withInsertData = (editor: Editor, rootDir: string | undefined, curr
         return
       }
     }
-
-    // If the text is a URL, insert a link instead of plain text.
-    if (isValidUrl(text)) {
-      const linkFragment: CustomText[] = [
-        {
-          text: text,
-          url: text,
-        }
-      ]
-      Editor.insertFragment(editor, linkFragment)
-      return
-    }
-
-    //  else if (isImageUrl(text)) {
-    //   insertImage(editor, text)
-    //   return
-    // }
 
     // Use the default insertData method for other types of data.
     insertData(data)
@@ -94,4 +88,56 @@ const insertImage = (editor: Editor, url: string) => {
   // Always wrap the image with a paragraph element, because parseMarkdownSourceToSlateNodes will always wrap the image with a paragraph element.
   Transforms.insertNodes(editor, { type: 'paragraph', children: [image] })
   Transforms.insertNodes(editor, DefaultParagraphElement())
+}
+
+const parseHtml = (html: string) => {
+    const domParser = new DOMParser();
+    const doc = domParser.parseFromString(html, 'text/html');
+
+    const traverse = (node: Node): any => {
+      // console.log('traverse node:', node)
+      if (node.nodeType === Node.TEXT_NODE) {
+        return { text: node.textContent || '' };
+      }
+      if (node.nodeType !== Node.ELEMENT_NODE) {
+        return null;
+      }
+      const element = node as HTMLElement;
+      let children: any[] = [];
+      element.childNodes.forEach(child => {
+        const childNode = traverse(child);
+        if (childNode) {
+          if (childNode.text !== undefined) {
+              children.push(childNode);
+          } else {
+              children = children.concat(childNode.children || []);
+          }
+      }
+      });
+      // console.log('element: ', element)
+      switch (element.tagName.toLowerCase()) {
+        case 'p':
+          return { type: 'paragraph', children };
+        case 'strong':
+        case 'b':
+          return { text: children.map((c) => c.text).join(''), bold: true };
+        case 'em':
+        case 'i':
+          return { text: children.map((c) => c.text).join(''), italic: true };
+        case 'a':
+          return { 
+            text: children.map((c) => c.text).join(''), 
+            url: element.getAttribute('href') || '' 
+        };
+        case 'img':
+          return { type: 'image', url: element.getAttribute('src') || '', children: [{ text: '' }] };
+        // handle other elements
+        default:
+          return { children };
+      }
+    };
+
+    const slateNodes = Array.from(doc.body.childNodes).map(traverse).filter(Boolean);
+    // console.log('slateNodes: ', slateNodes)
+    return slateNodes;
 }
