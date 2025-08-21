2ebbecce2fac4b28311a1b623c2d779cbda084aa
Isorkz <isorkz@outlook.com>
2025-08-21 19:15:03 +0800

    fix request entity too large issue when pasting image

--- web-server/src/app.ts
+++ web-server/src/app.ts
@@ -9,7 +9,7 @@ const app = express()
 
 // Middleware
 app.use(cors())
-app.use(express.json())
+app.use(express.json({ limit: '3mb' })) // By default, Express limits JSON body size to 100kb, we increase it to 3mb for image pasting
 
 // Serve static files (React frontend)
 // In development (ts-node): __dirname = /Users/.../web-server/src
